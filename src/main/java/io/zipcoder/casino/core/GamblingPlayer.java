9d1b8ad2294ecec0a2b01f51389c5a6e0dadaa2f
Peter <pjoberg13@gmail.com>
2020-11-18 13:35:09 -0500

    added player class

---
+++ src/main/java/io/zipcoder/casino/core/GamblingPlayer.java
@@ -0,0 +1,4 @@
+package io.zipcoder.casino.core;
+
+public interface GamblingPlayer {
+}
---
+++ src/main/java/io/zipcoder/casino/core/Player.java
@@ -0,0 +1,14 @@
+package io.zipcoder.casino.core;
+
+public class Player {
+
+    private double playerMoney;
+
+    public Player() {
+
+    }
+
+    public Player(double initialMoney) {
+
+    }
+}
