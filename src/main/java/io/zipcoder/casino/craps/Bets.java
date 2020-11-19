15c50b434b0e64196e0e50fbbcedfc1ea97f7c57
MBuzzard87 <62930119+MBuzzard87@users.noreply.github.com>
2020-11-19 07:40:57 -0500

    Update Bets.java

--- src/main/java/io/zipcoder/casino/craps/Bets.java
+++ src/main/java/io/zipcoder/casino/craps/Bets.java
@@ -1,40 +1,6 @@
 package io.zipcoder.casino.craps;
 
-import io.zipcoder.casino.core.GamblingGame;
-import io.zipcoder.casino.utilities.Console;
-
-public class Bets implements GamblingGame {
-    Console console;
-    Craps craps;
-    GamblingGame gamblingGame;
-    CrapsEngine crapsEngine;
-    private Integer betAmt;
-
-
-
-    public Integer passBet() {
-        if(craps.dieVal == 7 || dieVal == 11) {
-
-
-        }
-        return null;
-    }
-
-    public Integer dontPassBetDie() {
-        return null;
-    }
-
-
-    public void setBetAmt(Integer betAmt) {
-        this.betAmt = betAmt;
-    }
-
-
-    public double getPlayerBet(double playerBet) {
-        return ;
-    }
-
-    public Integer getBetAmt() {
-        return betAmt;
-    }
+public enum Bets {
+    PASSBET,
+    DONTPASSBET
 }
