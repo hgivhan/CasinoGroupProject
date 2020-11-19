f2475433f0837be63105b647787b08198fa63de7
hillary <hillary@zipcodes-MBP.home>
2020-11-19 07:44:41 -0500

    Ceelo Test Skeletons

---
+++ src/test/java/io/zipcoder/casino/Ceelo/CeeloGameTest.java
@@ -0,0 +1,31 @@
+package io.zipcoder.casino.Ceelo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CeeloGameTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void checkAutoWin() {
+    }
+
+    @Test
+    public void checkAutoLose() {
+    }
+
+    @Test
+    public void checkTriple() {
+    }
+
+    @Test
+    public void checkPoint() {
+    }
+}
---
+++ src/test/java/io/zipcoder/casino/Ceelo/CeeloMenuTest.java
@@ -0,0 +1,63 @@
+package io.zipcoder.casino.Ceelo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CeeloMenuTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void welcome() {
+    }
+
+    @Test
+    public void promptRoll() {
+    }
+
+    @Test
+    public void youWon() {
+    }
+
+    @Test
+    public void youLose() {
+    }
+
+    @Test
+    public void pointPrompt() {
+    }
+
+    @Test
+    public void triplePrompt() {
+    }
+
+    @Test
+    public void gotNothing() {
+    }
+
+    @Test
+    public void compTriple() {
+    }
+
+    @Test
+    public void compPoint() {
+    }
+
+    @Test
+    public void shootOut() {
+    }
+
+    @Test
+    public void tryAgain() {
+    }
+
+    @Test
+    public void playAgain() {
+    }
+}
