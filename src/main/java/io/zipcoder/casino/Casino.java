0356027adcb72d7752334df1434b864c9235efac
EricRinger6 <eric.ringer6@gmail.com>
2020-11-19 20:02:04 -0500

    Updated

--- src/main/java/io/zipcoder/casino/Casino.java
+++ src/main/java/io/zipcoder/casino/Casino.java
@@ -6,7 +6,6 @@ import io.zipcoder.casino.cardstuff.GoFish;
 public class Casino {
     public static void main(String[] args) {
         // write your tests before you start
-        GoFish goFish = new GoFish();
-        goFish.run();
+        
     }
 }
